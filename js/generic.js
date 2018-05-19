42178e60ac01921ff03c32dedfca98f4e7f2ae1a
Thompson <wl33thompson@gmail.com>
2018-05-19 17:04:27 +0800

    add signup.html

--- js/generic.js
+++ js/generic.js
@@ -1,3 +1,24 @@
+
 $(document).ready(function(){
     $('[data-toggle="tooltip"]').tooltip(); 
 });
+
+(function(){
+    var tnb = {};
+    tnb.SIGN_UP = 'Sign Up';
+    tnb.SIGN_IN = 'Sign In';
+    
+    tnb.redirect = function(button){
+        switch($(button).text()){
+            case tnb.SIGN_IN : 
+                window.location.href='login.html';
+                break;
+            case tnb.SIGN_UP :                 
+                window.location.href='signup.html';    
+                break;
+            default: break;
+        }
+    }
+    
+    window.tnb = tnb;
+})();
